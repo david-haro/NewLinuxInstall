f64d23d1d6942b880ec6075165f392d04a4baeaa
david-haro <david-haro@users.noreply.github.com>
2025-07-17 13:34:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #stuff to install
+
 sudo apt-get install screen
+
+#hardstatus off
+#hardstatus alwayslastline
+#hardstatus string '%{= .g} %H |%=%{K}%{= w}%?%{K}%-Lw%?%{r}(%{W}%{w}%n%{w}*%f%t%?(%u)%?%{r})%{w}%?%{K}%+Lw%?%= %{g}|%{B} %m-%d %{W}%c %{g} '
+
 sudo apt-get install ssh
  sudo apt-get install x11vnc
 
